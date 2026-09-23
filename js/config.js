d90665b1132e33a2e8e18606bc215fc9817e69f9
Cheigion <cheigion@gmail.com>
2026-09-23 14:18:04 -0300

    Update CSV URLs in config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 // ===== CONFIGURACIÓN: pegue aquí sus enlaces =====
 const CONFIG = {
   // Google Sheet "Condominio-usuarios" publicado como CSV
-  USERS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRH29F0OcvZv6LYON0itXDtDpjyDkMzccw2pjWUrdI1gIGOkevj7BP-Z7lLQlS4Tg/pub?gid=1659959888&single=true&output=csv",
+  USERS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vREYMXOOqNBLwrGPdrPbGTJhkwYJV-2DyEhfzLdnLtMRiqXwNbVmRHjiBrLRFbPmfpRBjeTAFTjMw7W/pub?gid=1659959888&single=true&output=csv",
   // Google Sheet "Condominio-registros" publicado como CSV
-  VISITS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfuQBitvjG8GxAhQSfqev97-Qr0qTeYSd9XLoITqVwg_LjsaAdQcfieydA81xa_g/pub?gid=398474764&single=true&output=csv",
+  VISITS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRDN42gSV6NuyI1MJYnlmtH_TpajF_sJMuoI6_zwiAyj89VPyPqeo4XGJXrtUtys92tsTXTG-EeRDDA/pub?gid=398474764&single=true&output=csv",
   // Endpoint (Custom webhook) de Make.com que agrega la fila en Condominio-registros
   MAKE_WEBHOOK_URL: "https://hook.us2.make.com/8w2anwqlot4gz8qb2gmpl0wg99acplpy",
   TIMEZONE: "America/Santiago",
